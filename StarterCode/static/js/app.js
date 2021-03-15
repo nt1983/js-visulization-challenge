b653ed38675fb5650614c68064b266f23a703250
nt1983 <73156205+nt1983@users.noreply.github.com>
2021-03-15 14:49:29 -0700

    latest update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,24 +1,28 @@
 
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
+function DrawPlot(Option) {
+  d3.json("../data/samples.json").then((data) =>{
+    var input=data.sample;
+    console.log(input);
+    //var datafilter=sample.filter(inputOption => inputOption.id===sample);
+  
 
+  });
+}
 
 
 function init() {
     var dropdown=d3.select("#selDataset");
     d3.json("../data/samples.json").then(function(data) {
         var SampleName=data.names;
-        console.log(SampleName);
-        
-    });
+        SampleName.forEach((item) => {
+          dropdown.append("option").text(item).property("value", item);
+        });
+        const FirstOption=SampleName[0];
+        DrawPlot(FirstOption);
+
+    });    
 }
-
-
-
-
+        
 
 
 
